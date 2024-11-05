cf317edee8b7b2eb5dbed8dec4c6b2aeba0d97ae
noga-legit <noga@legitsecurity.com>
2024-11-05 14:13:28 +0200

    Update unencrypted_EBS_volume.py

--- unencrypted_EBS_volume.py
+++ unencrypted_EBS_volume.py
@@ -1,3 +1,7 @@
+# AWS recommends encrypting EBS volumes to protect data at rest. Creating an unencrypted volume is a misconfiguration that can lead to data exposure 
+# if the volume is compromised. Here’s how to create an unencrypted volume.
+# Setting Encrypted=False results in an unencrypted EBS volume, which may lead to unauthorized access to sensitive data if the volume is leaked or accessed.
+
 ec2_client = boto3.client('ec2')
 
 try:
